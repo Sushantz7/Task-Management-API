a66e65e078e54a5f67e7831e5512bfff8c484024
Sushantz7 <sushantwork777@gmail.com>
2025-10-15 16:05:15 +0545

    feat: register models.

--- apps/TaskManager/admin.py
+++ apps/TaskManager/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project, Task, Comment
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("name", "owner", "created_at", "updated_at")
+
+
+admin.site.register(Project)
+admin.site.register(Task)
+admin.site.register(Comment)
