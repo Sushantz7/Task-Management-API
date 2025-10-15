1501370f110329e44ad5a9ffeb11a41006e391ef
Sushantz7 <sushantwork777@gmail.com>
2025-10-15 16:04:46 +0545

    feat: create Project,Task and Comment models.

--- apps/TaskManager/models.py
+++ apps/TaskManager/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth import settings
 
-# Create your models here.
+from utils.base_model import BaseModel
+from utils.constants import StatusChoices
+
+
+class Project(BaseModel):
+    name = models.CharField(max_length=255, blank=False, null=False)
+    description = models.TextField()
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="owned_projects",
+    )
+    members = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="member_projects"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Task(BaseModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    status = models.CharField(
+        max_length=11, choices=StatusChoices, default=StatusChoices.TODO
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="tasks")
+    assignee = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="assigned_tasks",
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(BaseModel):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments"
+    )
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.author.email} commented on {self.task.title}"
