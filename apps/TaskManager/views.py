f0b107e1cfcf4ece430345df3759e0663e63ec71
Sushantz7 <sushantwork777@gmail.com>
2025-10-25 21:31:58 +0545

    feat: create view for project list and create.

--- apps/TaskManager/views.py
+++ apps/TaskManager/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView
 
-# Create your views here.
+from .models import Project
+from .serializers import ProjectSerializer, ProjectCreateSerializer
+
+
+class ProjectListView(ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class ProjectCreateView(CreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectCreateSerializer
